96925fc52556d22006d956406d22babe38398d8a
katejoyreed <katejoyreed@gmail.com>
2021-03-03 11:39:20 -0600

    Added member variable and ctor to Sweepstakes class

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -8,5 +8,15 @@ namespace Sweepstakes
 {
     class Sweepstakes
     {
+        public Dictionary<int, Contestant> contestants;
+        private string name;
+        public string Name;
+
+        public Sweepstakes(string name)
+        {
+            this.name = name;
+            Name = name;
+            contestants = new Dictionary<int, Contestant>();
+        }
     }
 }
