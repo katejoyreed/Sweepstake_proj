9634a41a0efa8992cea1519b4ffdeb96a239e9aa
katejoyreed <katejoyreed@gmail.com>
2021-03-03 12:55:33 -0600

    Added provisional constructor to Contestant class incorporating the GetUserInput method inherited from the UserInterface class

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -12,5 +12,13 @@ namespace Sweepstakes
         public string lastName;
         public string emailAddress;
         public int registrationNumber;
+
+        public Contestant()
+        {
+            firstName = GetUserInput("What is your first name?");
+            lastName = GetUserInput("What is your last name?");
+            emailAddress = GetUserInput("Please enter your email address:");
+
+        }
     }
 }
